5e7b2a68caa28007089886841ad352dbcd7c1113
EMEA\IBA1SF <fixed-term.ahmad.ibraheem@bosch.com>
2024-04-16 17:18:42 +0200

    Add project files.

---
+++ PublisherConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using PublisherData;
+using PublisherDomain;
+
+PubContext _context = new(); //existing database
+
+//InsertNewAuthorWithNewBook();
+InsertBookForExistingAuthor();
+
+void InsertNewAuthorWithNewBook()
+{
+    var author = new Author { FirstName = "Lynda", LastName = "Rutledge" };
+    author.Books.Add(new Book
+    {
+        Title = "West With Giraffes",
+        PublishDate = new DateOnly(2021, 2, 1)
+    });
+    _context.Authors.Add(author);
+    _context.SaveChanges();
+}
+
+void InsertBookForExistingAuthor()
+{
+    Book book = new Book { Title = "Book3", PublishDate = new DateOnly(2016, 3, 7), BasePrice = 10, AuthorId = 3 };
+    _context.Books.Add(book);
+    _context.SaveChanges();
+}
